12d4098f95f7c252ec0b32504d859fee75e87b26
michelle :) <michelleeliasflores@gmail.com>
2023-09-23 16:04:27 -0400

    added the macbook stuff + trying something out

--- thesilly.py
+++ thesilly.py
@@ -2,6 +2,14 @@ import streamlit as st
 import numpy as np 
 import pandas as pd 
 import time 
+
+
+st.title("GetSpaced: Explore with Math")
+st.button("Cool AI Stuff if your bored", type="primary") 
+if st.button('Click me!'): 
+    st("Go to the next screen!")
+else: 
+    st.write("Okay bye!")
 progress_bar = st.progress(0)
 status_text = st.empty()
 chart = st.line_chart(np.random.randn(10, 2))
